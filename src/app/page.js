c79cd11df8e31e889880b2ed8ddee56f42ae8d35
parkbible <p8yft8903@gmail.com>
2024-06-01 01:20:16 +0900

    chore: 배경 디자인 수정 및 푸터 추가

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,25 @@
 "use client"
 import React from "react";
-import Image from "next/image";
 import Menu from "./menu/Menu";
 import About from "./about/About";
 import Tech from "./tech/Tech";
 import Project from "./project/Project";
 import Experience from "./experience/Experience";
 import Blog from "./blog/Blog";
+import Footer from "./footer/Footer";
 
 export default function Home() {
   return (
     <>
       <Menu/>
-      <div className="rounded-lg bg-white min-[1675px]:w-[65%] max-xl:w-full w-4/5 m-auto sm:mt-[140px] sm:mb-20 pt-[60px] pb-10 sm:px-20 px-5">
-        <About/>
+      <About/>
+      <div className="rounded-lg bg-white min-[1675px]:w-[65%] max-xl:w-full w-4/5 m-auto sm:mt-[100px] sm:mb-20 py-10 sm:px-20 px-5">
         <Tech/>
         <Project/>
         <Experience/>
         <Blog/>
       </div>
+      <Footer/>
     </>
   );
 }
