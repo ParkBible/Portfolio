7c620a21d5c3b64435d3ddc02ec83594f0935324
parkbible <p8yft8903@gmail.com>
2024-06-02 02:33:22 +0900

    feat: 메뉴바 디자인 변경, 모바일 비율 변경

--- src/app/menu/menu.jsx
+++ src/app/menu/menu.jsx
@@ -1,10 +1,12 @@
 import {Link} from "react-scroll";
 import { useEffect, useState } from "react";
 import Top from "../top/Top";
+import "./style.css"
 
 export default function Menu() {
   const [isVisible, setIsVisible] = useState(true);
   const [isTopBtnVisible, setIsTopBtnVisible] = useState(true);
+  const [hoveredIndex, setHoveredIndex] = useState(null);
 
   const items = ["About Me", "Tech Stack", "Project", "Experience", "Blog"];
   let topY = 0;
@@ -24,18 +26,20 @@ export default function Menu() {
 
   return (
     <>
-      <nav id="menubar" className={`${!isVisible ? "translate-y-[-75px]" : ""} flex w-full h-[60px] opacity-90 bg-light-blue justify-center items-center fixed z-20`}>
+      <nav id="menubar" className={`${!isVisible ? "translate-y-[-75px]" : ""} flex w-full h-[60px] opacity-90 bg-darkest justify-center items-center fixed z-20`}>
         <header className="flex gap-28 justify-between">
-          <ul className="flex gap-10 max-sm:text-sm text-lg">
+          <ul className="flex gap-10 max-sm:text-sm text-2xl">
             {
               items.map((item, index) => (
-                <li id="menu" className="cursor-pointer" key={index}>
+                <li id="menu" className="cursor-pointer text-white" key={index} onMouseEnter={() => setHoveredIndex(index)} onMouseLeave={() => setHoveredIndex(null)}>
                   <Link to={item} smooth={true} duration={500}>
                     {item}
                   </Link>
+                  <div id="menu-bottom-box" className={`${index === hoveredIndex ? "on" : ""} relative bottom-[11px] h-[9px] bg-dark z-[-999]`}></div>
                 </li>
               ))
             }
+            <img className="w-[25px] h-[25px] relative top-[3px] cursor-pointer" src="/images/github2.png" alt="github" onClick={() => location.href="https://github.com/parkbible"}/>
           </ul>
         </header>
       </nav>
--- src/app/title/Title.jsx
+++ src/app/title/Title.jsx
@@ -6,15 +6,15 @@ export default function Title(props) {
     const [isShow, setIsShow] = React.useState(false);
 
     return (
-        <div className="flex mb-2">
-                <span className="text-3xl text-darker">
-                    <p className="relative z-10 font-semibold">
-                        {props.name}
-                    </p>
-                    <InView onChange={setIsShow}>
-                        <div className={`line ${isShow ? "on" : ""} relative bg-light h-4 bottom-4`}/>
-                    </InView>
-                </span>
+        <div id="title" className="flex mb-2">
+            <span className="text-5xl text-darker">
+                <p className="relative z-10 font-extrabold">
+                    {props.name}
+                </p>
+                <InView onChange={setIsShow}>
+                    <div className={`line ${isShow ? "on" : ""} relative bg-light h-4 bottom-4`}/>
+                </InView>
+            </span>
         </div>
     )
 }
