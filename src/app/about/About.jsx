fe8c0c9b1813f43ab5b2ddab2e5389883a270111
ParkBible <p8yft8903@gmail.com>
2024-10-27 23:17:12 +0900

    프로젝트 설명 수정

--- src/app/about/About.jsx
+++ src/app/about/About.jsx
@@ -48,8 +48,8 @@ export default function About() {
                 </div>
                 <div className={`about description ${isDescriptionShow ? "on" : ""} text-center bg-light-blue rounded-md mt-3 p-3 text-darkest`}>
                     <div>
-                        <b className="text-darker">Frontend</b>를 심도 있게 다루지만,&nbsp; 
-                        <b className="text-darker">Full Stack 개발자</b>로서 경험을 쌓았던 만큼 Backend, DB, 서버 인프라에 이르기까지 폭넓은 시야를 가지고 일합니다.<br/>
+                        {/* <b className="text-darker">Frontend</b>를 심도 있게 다루지만,&nbsp;  */}
+                        <b className="text-darker">Full Stack 개발자</b>로서 경험을 쌓았던 만큼 Frontend, Backend, DB, 서버 인프라에 이르기까지 폭넓은 시야를 가지고 일합니다.<br/>
                         주어지는 업무와 작성한 코드에 끝까지 책임감을 가지며, 사용자에게 <b>즐겁고 편안한 기억을 주는 웹 솔루션</b>을 만드는 데 집중합니다.<br/>
                         좋은 기술과 더 나은 방법을 찾아 함께 협력하고 성장하는 기회를 만들어가고자 합니다.
                     </div>
--- src/app/project/Project.info.jsx
+++ src/app/project/Project.info.jsx
@@ -4,16 +4,16 @@ export const ProjectInfos = [
         description: "한국관광공사 협업 관광지 추천 웹 개발 프로젝트입니다. 공공API와 크롤링 데이터를 토대로 추천 웹 페이지를 구축하였습니다.",
         company: "한국관광공사, 빅리더 AI 아카데미 (데이터 청년 캠퍼스)",
         date: "2021.10.01 ~ 2021.12.31",
-        role: "프론트엔드, 웹 크롤링",
+        role: "Python BeautifulSoup을 이용한 숙박업소 리뷰 사이트 크롤링, HTML, CSS, JS를 통한 프론트엔드 개발",
         stacks: ["JavaScript", "Python"],
         git: "https://github.com/parkbible/KTO"
     },
     {
         title: '다이어리형 커뮤니티 사이트 "오늘 어때?"',
-        description: "로그인/회원가입과 추천, 댓글 기능이 포함되어 있는 게시판 프로젝트입니다.",
+        description: "CAPTCHA API를 통한 회원가입/로그인과 게시물 작성, 추천, 댓글 기능이 포함되어 있는 게시판 프로젝트입니다.",
         company: "부경대학교X멀티캠퍼스",
         date: "2022.01.27 ~ 2022.02.14",
-        role: "풀스택 개발(1인)",
+        role: "풀스택 개발(1인) - Spring MVC 패턴 이용, DAO 패턴을 이용하여 DB 로직과 비즈니스 로직을 분리하여 개발",
         stacks: ["JSP", "JQuery", "Spring"],
         git: "https://github.com/parkbible/Diary-Community"
     },
@@ -22,7 +22,7 @@ export const ProjectInfos = [
         description: "미세먼지 등 현재 대기 정보를 폴리곤 형식으로 지도에 표시하고 통계 정보를 차트로 제공하는 솔루션입니다.",
         company: "(주)아이지아이에스",
         date: "2022.10.18 ~ 2023.1.17",
-        role: "풀스택 개발(1인)",
+        role: "풀스택 개발(1인) - JPA, QueryDSL 사용 및 테스트 코드 작성, CentOS 서버에 배포 작업",
         stacks: ["TypeScript", "Angular", "Jasmine", "Java", "Spring_Boot", "Junit", "MariaDB"]
     },
     {
@@ -30,7 +30,7 @@ export const ProjectInfos = [
         description: "사내 제공 서비스들을 하나의 계정 정보로 접속할 수 있게 하는 통합 플랫폼입니다.",
         company: "(주)아이지아이에스",
         date: "2023.4.9 ~ ",
-        role: "풀스택 개발(3인) - Keycloak을 통한 SSO 로그인, 제품 소개 페이지, 강의 신청, lazy Loading 적용한 FAQ 페이지 개발",
+        role: "풀스택 개발(3인) - Keycloak을 통한 SSO 로그인, 제품 소개 페이지, 강의 신청, Lazy Loading 적용한 FAQ 페이지 개발",
         stacks: ["TypeScript", "React", "React_Testing_Library", "Kotlin", "Spring_Boot","Kotest", "GraphQL", "MariaDB"],
         web: "https://www.dfos.kr"
     },
@@ -39,15 +39,15 @@ export const ProjectInfos = [
         description: "드론 물류배송 및 화재/재난 알림 시스템입니다. 드론의 정보와 위치, 재난 정보를 지도상에서 실시간 파악하는 기능이 포함되었습니다.",
         company: "(주)아이지아이에스, 창원시",
         date: "2023.7.3 ~ 2023.8.18",
-        role: "풀스택 개발(2인) - 알림 기능을 제외한 모든 기능 개발, DB 설계",
+        role: "풀스택 개발(2인) - TDD 방법론으로 알림 기능을 제외한 모든 기능 개발, DB 설계, View 테이블 적극 활용",
         stacks: ["TypeScript", "React", "React_Testing_Library", "Kotlin", "Spring_Boot", "Kotest", "PostgreSQL"]
     },
     {
         title: "UAM 시뮬레이션",
-        description: "CesiumJS에서 제공하는 3D 지도와 3D 모델을 통해 드론의 움직임을 시뮬레이션하고 API에서 제공받은 날씨 정보를 정해진 시간마다 DB에 수집합니다.",
+        description: "CesiumJS에서 제공하는 3D 지도 위에 드론 3D 모델이 정해진 시나리오에 따라 움직이는 시뮬레이터입니다. 시뮬레이터는 배속, 되감기, 일시정지, 위치 초기화 기능을 제공하며, API에서 제공받은 날씨 정보를 정해진 시간마다 DB에 수집합니다.",
         company: "(주)아이지아이에스",
         date: "2023.11.21 ~ 2023.12.22",
-        role: "풀스택 개발(1인)",
+        role: "풀스택 개발(1인) - DB 테이블 설계, 스케줄링을 위한 서버 개발, CesiumJS 라이브러리를 활용한 국내 제공 솔루션 'Vworld'를 통해 3D 지도에 시뮬레이션을 구현",
         stacks: ["TypeScript", "JavaScript", "React", "Node.js_Express", "SQLite"]
     },
     {
@@ -55,7 +55,7 @@ export const ProjectInfos = [
         description: "드론이 촬영한 정사영상을 이용해 파노라마를 만들고 그 파노라마를 이용해 URL 형태로 공유 가능한 배포 산출물을 제작하는 시스템입니다.",
         company: "(주)아이지아이에스",
         date: "2024.1.17 ~ ",
-        role: "기존 프로젝트 유지보수 및 고도화 - 버그 수정, 산출물 저장 방식 개선, 탐색기 등 기능 추가",
+        role: "기존 프로젝트 유지보수 및 고도화 - 버그 수정, 산출물 저장 방식 개선, 탐색기, 엑셀 파일 브라우저에서 시각화 등 기능 추가",
         stacks: ["TypeScript", "Angular", "Spring_Boot", "MariaDB"],
         web: "https://panorama.dfos.kr/"
     },
@@ -82,7 +82,7 @@ export const ProjectInfos = [
         description: "기존 스테이션 관제 시스템의 개발환경, 코드상의 많은 문제점으로 인해 유지보수가 난해하다고 판단, 새로운 프로젝트로의 마이그레이션이 결정되었습니다.",
         company: "(주)아이지아이에스",
         date: "2024.10.4 ~ ",
-        role: "풀스택 개발(4인) - 필요한 통신 방법 정리 및 설계, 구현 (진행 중)",
+        role: "풀스택 개발(4인) - MQTT 통신을 통한 기기 원격 조작 기능(해당 기능에 필요한 클라이언트 퍼블리싱, 프론트엔드, API 서버 개발)",
         stacks: ["TypeScript", "React", "TailwindCSS", "Spring_Boot", "PostgreSQL"],
         web: "http://dev-dfos-station-viewer.igis.co.kr/"
     },
